ad0704a4abb44f8cda87568543815374af333bcd
harshashu <bodduharsha24@gmail.com>
2023-10-12 20:19:47 +0530

    update

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ import os
 from flask import Flask
 app = Flask(__name__)
 
-color = "red"
+color = os.environ.get('APP_COLOR')
+
 @app.route("/")
 def main():
-print(color)
+  print(color)
 return render_template('hello.html', color=color)
 if __name__ == "__main__":
 app.run(host="0.0.0.0", port="8080")
