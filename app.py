7815b27717cab8cf368a79a4386e17c33ee07e21
harshashu <bodduharsha24@gmail.com>
2023-10-12 22:05:50 +0530

    update

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ color = "red"
 
 @app.route("/")
 def main():
-    #return 'Hello'
-        return 'hello.html'
+    print(color)
+    return render_template('hello.html', color=color)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="8080")
