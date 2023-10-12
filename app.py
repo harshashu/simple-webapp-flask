2c791232abf1f6aa1b4748a2cd1a690d32632ffb
harshashu <bodduharsha24@gmail.com>
2023-10-12 22:39:15 +0530

    update

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import os
 from flask import Flask
 app = Flask(__name__)
+...
+...
+
+color ="red"
 
 @app.route("/")
 def main():
-    return "Welcome!"
+   print(color)
+return render_template('hello.html', color=color)
 
-@app.route('/how are you')
-def hello():
-    return 'I am good, how about you?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
